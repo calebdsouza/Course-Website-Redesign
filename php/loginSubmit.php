2aa8d0dba005e066cbb58f6fbdaba9b903321d2c
calebdsouza <ca.ds201@gmail.com>
2018-03-29 18:37:49 -0400

    Fixed db table name error causing inccorect display of username in index.php page

--- php/loginSubmit.php
+++ php/loginSubmit.php
@@ -6,27 +6,44 @@
     $username = mysqli_real_escape_string($db,$_POST['username']);
     $password = mysqli_real_escape_string($db,$_POST['password']); 
 
-    $sql = "SELECT ID, Type FROM  ".TABLE." WHERE UTORid = '$username' and password = '$password'";      
-	  $result = mysqli_query($db,$sql);
+    $sql = "SELECT ID, Type FROM  ".$table." WHERE UTORid = '$username' and password = '$password'";      
+	  $result = mysqli_query($db, $sql);
 
     $row = mysqli_fetch_array($result, MYSQLI_ASSOC);
   
     $count = mysqli_num_rows($result);
     
     $error = "";
+
     // If result matched $username and $password, table row must be 1 row
     if($count == 1) {
       $_SESSION['valid'] = true;
       $_SESSION['timeout'] = time();
-      if ($row['Type'] == 'S') {
+
+      // Check the account type of the user which has logged in 
+      if ($row['Type'] == 'S') { // Is a Student
         $table = "Student";
-        $sql = "SELECT FIRST, LAST FROM  ".TABLE." WHERE ID = ".$row["ID"];
-        $result = mysqli_query($db,$sql); 
+        $sql = "SELECT FIRST, LAST FROM  ".$table." WHERE ID = ".$row["ID"];
+        $result = mysqli_query($db, $sql); 
+        $row = mysqli_fetch_array($result, MYSQLI_ASSOC);
+        $_SESSION['user_name'] = $row["First"]." ".$row["LAST"];
+      } else if ($row['Type'] == 'T') { // Is a Teaching Assitant
+        $table = "T.A";
+        $sql = "SELECT FIRST, LAST FROM  ".$table." WHERE ID = ".$row["ID"];
+        $result = mysqli_query($db, $sql); 
+        $row = mysqli_fetch_array($result, MYSQLI_ASSOC);
+        $_SESSION['user_name'] = $row["First"]." ".$row["LAST"]; 
+      } else if ($row['Type'] == 'I') { // Is a Instructor
+        $table = "Instructor";
+        $sql = "SELECT FIRST, LAST FROM  ".$table." WHERE ID = ".$row["ID"];
+        $result = mysqli_query($db, $sql); 
         $row = mysqli_fetch_array($result, MYSQLI_ASSOC);
         $_SESSION['user_name'] = $row["First"]." ".$row["LAST"];
       }
+
       header("Location: ../index.php");
-    }else {
+      $_SESSION['error'] = "";
+    } else {
       $_SESSION['valid'] = false;
       $_SESSION['error'] = "Your Utorid or Password is invalid";
       header("Location: ../login.php");
