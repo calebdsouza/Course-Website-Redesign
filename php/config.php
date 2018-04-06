940b4439a1ff8c5dbbe9874e36c632c0a80ca4da
Michael <michaelyuan.sun@mail.utoronto.ca>
2018-04-05 21:22:47 -0400

    Changed config.php so can't define multiple times

--- php/config.php
+++ php/config.php
@@ -1,8 +1,8 @@
 <?php
-   if (!defined('DB_SERVER', 'mathlab.utsc.utoronto.ca')) define('DB_SERVER', 'mathlab.utsc.utoronto.ca');
-   if (!defined('DB_USERNAME', 'sunyuan8')) define('DB_USERNAME', 'sunyuan8');
-   if (!defined('DB_PASSWORD', '1243')) define('DB_PASSWORD', '1243');
-   if (!defined('DB_DATABASE', 'cscb20w18_sunyuan8')) define('DB_DATABASE', 'cscb20w18_sunyuan8');
+   if (!defined('DB_SERVER')) define('DB_SERVER', 'mathlab.utsc.utoronto.ca');
+   if (!defined('DB_USERNAME')) define('DB_USERNAME', 'sunyuan8');
+   if (!defined('DB_PASSWORD')) define('DB_PASSWORD', '1243');
+   if (!defined('DB_DATABASE')) define('DB_DATABASE', 'cscb20w18_sunyuan8');
    
    // Create connection to the database
    $db = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_DATABASE);
